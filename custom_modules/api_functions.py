8ca798d3e5c78f7847974f7b089b784667ed2532
Arie Twigt <twigt.arie@gmail.com>
2023-09-26 13:56:52 +0200

    added example for sub columns data frame

--- custom_modules/api_functions.py
+++ custom_modules/api_functions.py
@@ -4,8 +4,11 @@ import requests
 # function to import data from the rdw
 def import_cars_by_brand(brand: str) -> list:
 
+    # uppercase the brand
+    brand_upper = brand.upper()
+
     # define the endpoint
-    endpoint = "https://opendata.rdw.nl/resource/m9d7-ebf2.json"
+    endpoint = f"https://opendata.rdw.nl/resource/m9d7-ebf2.json?merk={brand_upper}"
     
     # execute the request
     response = requests.get(endpoint)
@@ -13,4 +16,8 @@ def import_cars_by_brand(brand: str) -> list:
     # get the data from the response
     data = response.json()
     
+    # check if the data is not empty
+    if len(data) == 0:
+        raise ValueError(f"Did not find any cars for {brand}")
+
     return data
--- custom_modules/conversion_functions.py
+++ custom_modules/conversion_functions.py
@@ -6,4 +6,24 @@ def convert_list_to_df(list_to_convert: list) -> pd.DataFrame:
     Accepts a list and converts it to a pandas DataFrame
     
     '''
+
+    # convert the list to a pandas DataFrame
+    df = pd.DataFrame(list_to_convert)
+
+    # analyse methoden
+    # eerste 5 regels laten zien
+    #df.head()
+
+    # overzicht van kolommen en data types
+    #df.info()
+
+    # overzicht van kolommen
+    #df.columns
+
+    # selecting a column 
+    #df['merk']
+
+    # create list of columns
+    selected_columns = ['kenteken', 'merk', 'handelsbenaming', 'catalogusprijs', 'datum_tenaamstelling']
+    df_sub_cols = df[selected_columns]
     pass
